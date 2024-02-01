81d6af973ec180e9f4d28b499848c4dc53362df7
DavBaquero <david.baquero.19@gmail.com>
2024-02-01 12:13:25 +0100

    🔧 feat(score.py): add function __init__ and ScoreHitPingsTotal

--- src/score.py
+++ src/score.py
@@ -1,2 +1,11 @@
-class Score:
+class ScoreCard:
+
+    def __init__(self, scorecard):
+        self.pins = scorecard
+        self.last_frame = 10
+        self.frame = 1
+        self.score = 0
     
+    def ScoreHitPingsTotal(self):
+        if 
+
